415b8e728dbc16db6cf0e2c408219dc54ffc36a2
Azat <aelchikushuulu@gmail.com>
2023-04-08 00:11:05 +0600

    Validator

--- src/WrongLoginException.java
+++ src/WrongLoginException.java
@@ -1,6 +1,2 @@
-public class WrongLoginException extends Exception{
-
-    public WrongLoginException(String message) {
-        super(message);
-    }
+public class WrongLoginException {
 }
--- src/WrongPasswordException.java
+++ src/WrongPasswordException.java
@@ -1,5 +1,2 @@
-public class WrongPasswordException extends Exception{
-    public WrongPasswordException(String message) {
-        super(message);
-    }
+public class WrongPasswordException {
 }
